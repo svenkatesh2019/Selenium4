391b1ab38e345b910e89f17c19e6b07ac75100a8
svenkatesh2019 <sridevivenkatesh@outlook.com>
2021-03-02 22:00:41 -0500

    added one relativelocator code

--- src/test/java/testcases/RelativeLocators.java
+++ src/test/java/testcases/RelativeLocators.java
@@ -20,7 +20,7 @@ public class RelativeLocators {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://way2automation.com/way2auto_jquery/index.php");
 		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("sud.v@hotmail.com");
-		// driver.findElement(RelativeLocator.withTagName("input").toRightOf(By.linkText("Signin"))).click();
+		driver.findElement(RelativeLocator.withTagName("input").toRightOf(By.linkText("Signin"))).click();
 		// driver.findElement(RelativeLocator.withTagName("input").toRightOf(By.xpath("//p[contains(text(),'Already
 		// Have')]"))).click();
 //		driver.findElement(withTagName("input")
@@ -28,7 +28,6 @@ public class RelativeLocators {
 		driver.findElement(RelativeLocator.withTagName("input")
 				.below(By.xpath("(//input[@name='password'][@type='password'])[2]"))).click();
 
-		
 	}
 
 }
